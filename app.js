5cfd8fa9c7acb741807845fdca6e4c3ece7fb5ad
rainstorm <alexandra_mitu98@yahoo.com>
2021-12-13 14:39:19 +0000

    final

--- app.js
+++ app.js
@@ -17,3 +17,23 @@ app.listen(app.get("port"), function(){
 })
 
 
+const request = require('request');
+const argv = require('yargs').argv;
+
+/*
+let apiKey = 'ffb92f5691371697cb699c3a235cc350'; //api key obtained from OpenWeather API
+let city = argv.c || 'bucharest';
+let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+
+request(url, function (err, response, body) {
+  if(err){
+    console.log('error:', error);
+  } 
+  
+  else {
+    let weather = JSON.parse(body);
+    let message = `It's ${weather.main.temp} degrees in ${weather.name}.`;
+    console.log(message);
+  }
+});
+*/
